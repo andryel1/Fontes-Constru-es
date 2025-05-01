2e0f555f409dd620c7a393ffe804bd911d4df84b
Andryel <andryelbonfim01@gmail.com>
2025-04-30 23:04:38 -0300

    adicionei wireframes

--- Application/Domain/Entities/Usuario.cs
+++ Application/Domain/Entities/Usuario.cs
@@ -6,5 +6,8 @@ namespace Documentos.PROJETOPESSOAL.Fontes_Constru_es.Domain.Entities
         public string Password { get; set; }
         public string Email { get; set; }
         public DateTime DataNascimento { get; set; }
+        
+        public Cliente Cliente { get; set; }
+        public Administrador Administrador { get; set; }
     }
 }
