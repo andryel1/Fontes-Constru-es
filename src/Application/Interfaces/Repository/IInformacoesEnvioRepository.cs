a4d1ed533b984b7a7c5584a7c78d507f6da400ab
Andryel <andryelbonfim01@gmail.com>
2025-05-20 22:42:46 -0300

    feat: update IInformacoesEnvioRepository

--- src/Application/Interfaces/Repository/IInformacoesEnvioRepository.cs
+++ src/Application/Interfaces/Repository/IInformacoesEnvioRepository.cs
@@ -2,8 +2,12 @@ using Application.Dtos;
 using Ecommerce.Domain.Entities;
 
 namespace Application.Interfaces.Repository;
+
 public interface IInformacoesEnvioRepository : IInterfaceBaseRepository<InformacoesEnvioDto, InformacoesEnvio>
 {
-    Task<InformacoesEnvioDto> ObterInformacoesEnvioPorId(int id);
-    Task<List<InformacoesEnvioDto>> ObterTodasInformacoesEnvio();
+    Task<InformacoesEnvioDto> ObterInformacoesEnvioPorId(Guid id);
+    Task<IEnumerable<InformacoesEnvioDto>> ObterTodosInformacoesEnvio();
+    Task<bool> AdicionarInformacoesEnvio(InformacoesEnvioDto informacoesEnvioDto);
+    Task<bool> AtualizarInformacoesEnvio(InformacoesEnvioDto informacoesEnvioDto);
+    Task<bool> DeletarInformacoesEnvio(Guid id);
 }
