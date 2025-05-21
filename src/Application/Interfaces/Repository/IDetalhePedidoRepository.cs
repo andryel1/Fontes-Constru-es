f5ef8cd2dafb6d4da06a32bb08628f62267576b2
Andryel <andryelbonfim01@gmail.com>
2025-05-20 22:42:46 -0300

    feat: update IDetalhePedidoRepository

--- src/Application/Interfaces/Repository/IDetalhePedidoRepository.cs
+++ src/Application/Interfaces/Repository/IDetalhePedidoRepository.cs
@@ -3,11 +3,10 @@ using Ecommerce.Domain.Entities;
 
 
 namespace Application.Interfaces.Repository;
-public interface IDetalhePedidoRepository
+
+public interface IDetalhePedidoRepository : IInterfaceBaseRepository<DetalhePedidoDto, DetalhePedido>
 {
-    Task<DetalhePedidoDto> AdicionarDetalheDoPedido(DetalhePedidoDto detalheDoPedido);
-    Task<DetalhePedidoDto> AtualizarDetalheDoPedido(DetalhePedidoDto detalheDoPedido);
-    Task<bool> DeletarDetalheDoPedido(int id);
-    Task<DetalhePedidoDto> ObterDetalheDoPedidoPorId(int id);
-    Task<List<DetalhePedidoDto>> ObterTodosDetalhesDoPedido();
+    Task<DetalhePedidoDto> ObterDetalhePedidoPorId(int id);
+    Task<IEnumerable<DetalhePedidoDto>> ObterDetalhesPedidoPorPedidoId(int pedidoId);
+
 }
