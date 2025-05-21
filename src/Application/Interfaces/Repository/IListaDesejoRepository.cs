8992c7be57d443fdcbf566a7b755093f210fdd67
Andryel <andryelbonfim01@gmail.com>
2025-05-20 22:42:46 -0300

    feat: update IListaDesejoRepository

---
+++ src/Application/Interfaces/Repository/IListaDesejoRepository.cs
@@ -0,0 +1,13 @@
+using Application.Dtos;
+using Ecommerce.Domain.Entities;
+
+namespace Application.Interfaces.Repository;
+
+public interface IListaDesejoRepository : IInterfaceBaseRepository<ListaDesejoDto, ListaDesejo>
+{
+
+    Task<ListaDesejoDto> ObterListaDesejoPorUsuario(Guid usuarioId);
+    Task<bool> VerificaSeProdutoJaEstaNaLista(Guid usuarioId, Guid produtoId);
+    Task<bool> AdicionarProdutoNaLista(Guid usuarioId, Guid produtoId);
+    Task<bool> RemoverProdutoDaLista(Guid usuarioId, Guid produtoId);
+}
