0d3cfebbdf1fd8b488c631d52a98b509e0632487
Andryel <andryelbonfim01@gmail.com>
2025-05-20 22:42:46 -0300

    feat: add IImagemService

---
+++ src/Application/Interfaces/Services/IImagemService.cs
@@ -0,0 +1,9 @@
+using Application.Dtos;
+using Application.Interfaces.Repository;
+using Ecommerce.Domain.Entities;
+
+namespace Application.Interfaces.Services;
+public interface IImagemService
+{
+
+}
