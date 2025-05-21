185c0426502e0a7cd941426bcd66821281aa9f37
Andryel <andryelbonfim01@gmail.com>
2025-05-20 22:42:46 -0300

    feat: add IAvaliacaoService

---
+++ src/Application/Interfaces/Services/IAvaliacaoService.cs
@@ -0,0 +1,10 @@
+using Application.Dtos;
+using Application.Interfaces.Repository;
+using Ecommerce.Domain.Entities;
+
+namespace Application.Interfaces.Services;
+
+public interface IAvaliacaoService : IAvaliaçaoRepository
+{
+
+}
