040a1d581519bd279963be0f72048667d24291ce
Andryel <andryelbonfim01@gmail.com>
2025-05-20 22:42:46 -0300

    feat: add ICategoriaService

---
+++ src/Application/Interfaces/Services/ICategoriaService.cs
@@ -0,0 +1,10 @@
+using Application.Dtos;
+using Ecommerce.Domain.Entities;
+using Application.Interfaces.Repository;
+
+namespace Application.Interfaces.Repository;
+
+public interface ICategoriaService
+{
+
+}
