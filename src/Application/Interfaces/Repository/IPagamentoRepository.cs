03422e637e711f5d8a5ee1d84e8dbaafbf5c80c2
Andryel <andryelbonfim01@gmail.com>
2025-05-20 22:42:46 -0300

    feat: update IPagamentoRepository

---
+++ src/Application/Interfaces/Repository/IPagamentoRepository.cs
@@ -0,0 +1,10 @@
+using Application.Dtos;
+using Ecommerce.Domain.Entities;
+
+namespace Application.Interfaces.Repository;
+
+public interface IPagamentoRepository : IInterfaceBaseRepository<PagamentoDto, Pagamento>
+{
+    Task<PagamentoDto> ObterPagamentoPorPedidoId(int pedidoId);
+    Task<bool> VerificarSePagamentoJaFoiRealizado(int pedidoId);
+}
