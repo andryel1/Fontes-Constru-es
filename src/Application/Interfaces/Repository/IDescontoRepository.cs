df4c35640bf284619b2ce0d97c0a08415dcc981f
Andryel <andryelbonfim01@gmail.com>
2025-05-20 22:42:46 -0300

    feat: update IDescontoRepository

--- src/Application/Interfaces/Repository/IDescontoRepository.cs
+++ src/Application/Interfaces/Repository/IDescontoRepository.cs
@@ -5,5 +5,10 @@ namespace Application.Interfaces.Repository;
 
 public interface IDescontoRepository : IInterfaceBaseRepository<DescontoDto, Desconto>
 {
+    Task<DescontoDto> ObterDescontoPorId(int id);
+    Task<IEnumerable<DescontoDto>> ObterDescontosPorProdutoId(int produtoId);
+    Task<IEnumerable<DescontoDto>> ObterDescontosAtivos();
+    Task<IEnumerable<DescontoDto>> ObterDescontosInativos();
+    Task<IEnumerable<DescontoDto>> ObterDescontosExpirados();
     
 }
