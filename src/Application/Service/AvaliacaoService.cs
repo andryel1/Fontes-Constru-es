8c8785fe55b69ba4d902c1d2bc48c92d402fbded
Andryel <andryelbonfim01@gmail.com>
2025-05-23 17:21:24 -0300

    chore: atualiza AvaliacaoService

--- src/Application/Service/AvaliacaoService.cs
+++ src/Application/Service/AvaliacaoService.cs
@@ -1,14 +1,9 @@
-using Application.Interfaces.Services;
+using Application.Interfaces.Service;
 
 namespace Application.Service
 {
-    public class AvaliacaoService : IAvaliacaoService
+    public class AvaliacaoService(IAvaliacaoService avaliacaoService)
     {
-        private readonly IAvaliacaoService _avaliacaoService;
-
-        public AvaliacaoService(IAvaliacaoService avaliacaoService)
-        {
-            _avaliacaoService = avaliacaoService;
-        }
+        private readonly IAvaliacaoService _avaliacaoService = avaliacaoService;
     }
 }
