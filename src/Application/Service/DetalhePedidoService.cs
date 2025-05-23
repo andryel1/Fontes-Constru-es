aeb0fb854f986ef2651fec19eb18999f19ae28c3
Andryel <andryelbonfim01@gmail.com>
2025-05-23 17:21:24 -0300

    chore: atualiza DetalhePedidoService

--- src/Application/Service/DetalhePedidoService.cs
+++ src/Application/Service/DetalhePedidoService.cs
@@ -1,14 +1,61 @@
-using Application.Interfaces.Services;
+using Application.Interfaces.Service;
+using Application.Dtos;
 
-namespace application.service
+namespace Application.Service
 {
-    public class CarrinhoService : ICarrinhoService
+    public class DetalhePedidoService(IDetalhePedidoService detalhePedido) : IDetalhePedidoService
     {
-        private readonly ICarrinhoService _carrinhoService;
+        private readonly IDetalhePedidoService _detalhePedido = detalhePedido;
 
-        public CarrinhoService(ICarrinhoService carrinhoService)
+        public Task<DescontoDto> ObterDescontoPorId(int id)
         {
-            _carrinhoService = carrinhoService;
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DescontoDto>> ObterDescontosPorProdutoId(int produtoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DescontoDto>> ObterDescontosAtivos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DescontoDto>> ObterDescontosInativos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DescontoDto>> ObterDescontosExpirados()
+        {
+            throw new NotImplementedException();
+        }
+
+        // IInterfaceBaseRepository<DescontoDto, Desconto> members
+        public Task<DescontoDto> Adicionar(DescontoDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DescontoDto> Atualizar(DescontoDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Deletar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DescontoDto> ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<DescontoDto>> ObterTodos()
+        {
+            throw new NotImplementedException();
         }
     }
 }
