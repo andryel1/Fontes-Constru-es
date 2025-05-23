0f26ea789ac59315130f996cee4b8dbcc9e0b52e
Andryel <andryelbonfim01@gmail.com>
2025-05-23 17:21:24 -0300

    chore: atualiza ProdutoService

--- src/Application/Service/ProdutoService.cs
+++ src/Application/Service/ProdutoService.cs
@@ -1,14 +1,64 @@
 using Application.Dtos;
-using Application.Interfaces.Services;
+using Application.Interfaces.Service;
 
 namespace Application.Service;
 
-public class ProdutoService
+public class ProdutoService(IProdutoService produto) : IProdutoService
 {
-    private readonly IProdutoService _produto;
+    private readonly IProdutoService _produto = produto;
 
-    public ProdutoService(IProdutoService produto)
+    public Task<ProdutoDto> VerificarProdutoExistente(string nome)
     {
-        _produto = produto;
+        return _produto.VerificarProdutoExistente(nome);
+    }
+
+    public Task<ProdutoDto> NomeNaoPodeSerVazioOuNUlo(string nome)
+    {
+        return _produto.NomeNaoPodeSerVazioOuNUlo(nome);
+    }
+
+    public Task<ProdutoDto> DescricaoNaoPodeSerVazioOuNulo(string descricao)
+    {
+        return _produto.DescricaoNaoPodeSerVazioOuNulo(descricao);
+    }
+
+    public Task<ProdutoDto> PrecoNaoPodeSerVazioOuNulo(decimal preco)
+    {
+        return _produto.PrecoNaoPodeSerVazioOuNulo(preco);
+    }
+
+    public Task<List<ProdutoDto>> ObterProdutosPorCategoria(string categoria)
+    {
+        return _produto.ObterProdutosPorCategoria(categoria);
+    }
+
+    public Task<List<ProdutoDto>> ObterProdutosPorNome(string nome)
+    {
+        return _produto.ObterProdutosPorNome(nome);
+    }
+
+    public Task<ProdutoDto> Adicionar(ProdutoDto dto)
+    {
+        return _produto.Adicionar(dto);
+    }
+
+    public Task<ProdutoDto> Atualizar(ProdutoDto dto)
+    {
+        return _produto.Atualizar(dto);
+    }
+
+    public Task<bool> Deletar(int id)
+    {
+        return _produto.Deletar(id);
+    }
+
+    public Task<ProdutoDto> ObterPorId(int id)
+    {
+        return _produto.ObterPorId(id);
+    }
+
+    public Task<List<ProdutoDto>> ObterTodos()
+    {
+        return _produto.ObterTodos();
     }
 }
