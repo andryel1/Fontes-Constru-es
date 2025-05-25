e5fdf9d3b2ea29bf5d019f196d415f89aaa2770c
Andryel <andryelbonfim01@gmail.com>
2025-05-25 20:13:10 -0300

    Adicionado EnviarEmailDto.cs

---
+++ src/Application/Dtos/EnviarEmailDto.cs
@@ -0,0 +1,10 @@
+namespace Application.Dtos
+{
+    public class EnviarEmailDto
+    {
+        public string? Nome { get; set; }
+        public string? Assunto { get; set; }
+        public string? Email { get; set; }
+        public string? Mensagem { get; set; }
+    }
+}
