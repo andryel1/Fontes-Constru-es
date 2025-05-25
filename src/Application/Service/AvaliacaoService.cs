6a015bde59434d097050e0ea8fa8a78a1dafaccd
Andryel <andryelbonfim01@gmail.com>
2025-05-25 20:13:10 -0300

    Modificado AvaliacaoService.cs

--- src/Application/Service/AvaliacaoService.cs
+++ src/Application/Service/AvaliacaoService.cs
@@ -1,9 +1,44 @@
+using Application.Dtos;
 using Application.Interfaces.Service;
 
 namespace Application.Service
 {
-    public class AvaliacaoService(IAvaliacaoService avaliacaoService)
+    public class AvaliacaoService(IAvaliacaoService avaliacaoService) : IAvaliacaoService
     {
         private readonly IAvaliacaoService _avaliacaoService = avaliacaoService;
+
+       public Task<AvaliacaoDto> Adicionar(AvaliacaoDto entity)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<AvaliacaoDto> Atualizar(AvaliacaoDto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Deletar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<AvaliacaoDto>> ObterAvaliacoesPorProdutoId(int produtoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<AvaliacaoDto>> ObterAvaliacoesPorUsuarioId(int usuarioId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AvaliacaoDto> ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<AvaliacaoDto>> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
