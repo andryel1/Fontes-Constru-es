9771c612514478b862853135883409ed36a91da1
Andryel <andryelbonfim01@gmail.com>
2025-05-25 20:13:10 -0300

    Adicionado RegistroDto.cs

---
+++ src/Application/Dtos/RegistroDto.cs
@@ -0,0 +1,9 @@
+namespace Application.Dtos;
+
+public class RegistroDto
+{
+    public string? Nome { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public string? ConfirmarPassword { get; set; }
+}
