42abbcd229c54aae235c200be92821d8ebdd9262
Andryel <andryelbonfim01@gmail.com>
2025-05-25 20:13:10 -0300

    Modificado AdministradorService.cs

--- src/Application/Service/AdministradorService.cs
+++ src/Application/Service/AdministradorService.cs
@@ -2,7 +2,7 @@ using Application.Interfaces.Service;
 
 namespace Application.Service
 {
-    public class AdministradorService(IAdministradorService administradorService)
+    public class AdministradorService(IAdministradorService administradorService) : IAdministradorService
     {
         private readonly IAdministradorService _administradorService = administradorService;
     }
