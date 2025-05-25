4620b6f6b5260d27d91affe72f83c827d075cffc
Andryel <andryelbonfim01@gmail.com>
2025-05-25 20:13:10 -0300

    Modificado IPagamentoRepository.cs

--- src/Application/Interfaces/Repository/IPagamentoRepository.cs
+++ src/Application/Interfaces/Repository/IPagamentoRepository.cs
@@ -1,6 +1,7 @@
 using Application.Dtos;
 using Ecommerce.Domain.Entities;
 
+
 namespace Application.Interfaces.Repository;
 
 public interface IPagamentoRepository : IInterfaceBaseRepository<PagamentoDto, Pagamento>
