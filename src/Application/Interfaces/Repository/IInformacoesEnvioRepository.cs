c41364e79b2932f5b19651b3113337baac915321
Andryel <andryelbonfim01@gmail.com>
2025-05-31 13:12:56 -0300

    Modificado IInformacoesEnvioRepository.cs

--- src/Application/Interfaces/Repository/IInformacoesEnvioRepository.cs
+++ src/Application/Interfaces/Repository/IInformacoesEnvioRepository.cs
@@ -5,9 +5,9 @@ namespace Application.Interfaces.Repository;
 
 public interface IInformacoesEnvioRepository : IInterfaceBaseRepository<InformacoesEnvioDto, InformacoesEnvio>
 {
-    Task<InformacoesEnvioDto> ObterInformacoesEnvioPorId(Guid id);
+    Task<InformacoesEnvioDto> ObterInformacoesEnvioPorId(int id);
     Task<IEnumerable<InformacoesEnvioDto>> ObterTodosInformacoesEnvio();
     Task<bool> AdicionarInformacoesEnvio(InformacoesEnvioDto informacoesEnvioDto);
     Task<bool> AtualizarInformacoesEnvio(InformacoesEnvioDto informacoesEnvioDto);
-    Task<bool> DeletarInformacoesEnvio(Guid id);
+    Task<bool> DeletarInformacoesEnvio(int id);
 }
