8fc4c445ee95fb21292a6cc5a0e2007fdc780b12
Andryel <andryelbonfim01@gmail.com>
2025-06-07 23:53:45 -0300

    feat: adiciona validador de Informações de Envio

---
+++ src/Application/Interfaces/Validacao/InformacoesEnvioValidator.cs
@@ -0,0 +1,11 @@
+using Application.Dtos;
+using Resources;
+using FluentValidation;
+
+public class InformacoesEnvioValidator : AbstractValidator<InformacoesEnvioDto>
+{
+    public InformacoesEnvioValidator()
+    {
+        
+    }
+}
