d1a55c636f118d655105cd12cbfdea49d2a303e6
Andryel <andryelbonfim01@gmail.com>
2025-06-07 23:53:45 -0300

    feat: adiciona validador de Usuário

---
+++ src/Application/Interfaces/Service/UsuarioValidator.cs
@@ -0,0 +1,12 @@
+using Resources;
+using Application.Dtos;
+using FluentValidation;
+using Ecommerce.Domain.Entities;
+
+public class UsuarioValidator : AbstractValidator<AvaliacaoDto>
+{
+    public UsuarioValidator()
+    {
+        
+    }
+}
