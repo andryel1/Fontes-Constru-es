2051d2fbd9c2162d97d464fc5e64180b51abade0
Andryel <andryelbonfim01@gmail.com>
2025-06-09 23:45:36 -0300

    feat-implementação de messages

---
+++ src/Infrastructure/Context.cs
@@ -0,0 +1,18 @@
+﻿namespace Infrastructure
+{
+    public class Context : DbContext
+    {
+        // Construtor que recebe as opções do contexto
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
