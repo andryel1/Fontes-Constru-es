5748eb665b9ac1f384924ee0bc8ff11a2a43cec9
Andryel <andryelbonfim01@gmail.com>
2025-06-16 06:05:28 -0300

    chore: atualizações no contexto EF e csproj de infraestrutura

--- src/Infrastructure/Context.cs
+++ src/Infrastructure/Context.cs
@@ -15,16 +15,18 @@ namespace Infrastructure
         public DbSet<Avaliacao> Avaliacoes { get; set; }
         public DbSet<Carrinho> Carrinhos { get; set; }
         public DbSet<ItemCarrinho> ItensCarrinho { get; set; }
+        public DbSet<ListaDesejo> ListasDesejo { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            
+
             modelBuilder.ApplyConfiguration(new AvaliacaoConfiguration());
             modelBuilder.ApplyConfiguration(new CarrinhoConfiguration());
             modelBuilder.ApplyConfiguration(new PedidoConfiguration());
             modelBuilder.ApplyConfiguration(new ProdutoConfiguration());
             modelBuilder.ApplyConfiguration(new TagConfiguration());
+            modelBuilder.ApplyConfiguration(new ListaDesejoConfiguration());
         }
     }
 }
