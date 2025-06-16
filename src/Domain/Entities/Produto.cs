52b92db8e4cd8381813fe5f7135ef2ff8e07a80f
Andryel <andryelbonfim01@gmail.com>
2025-06-16 01:57:14 -0300

    feat: atualiza entidade Produto

--- src/Domain/Entities/Produto.cs
+++ src/Domain/Entities/Produto.cs
@@ -2,16 +2,16 @@ namespace Ecommerce.Domain.Entities
 {
     public class Produto : Entitybase
     {
-        public string Nome { get; set; }
-        public string Descricao { get; set; }
-        public string Estoque { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public string? Estoque { get; set; }
         public decimal Preco { get; set; }
-        public ICollection<Imagem> Imagens { get; set; }
-        public ICollection<Avaliacao> Avaliacoes { get; set; }
-        public ICollection<ItemCarrinho> ItensCarrinho { get; set; }
-        public ICollection<Pedido> Pedidos { get; set; }
-        public ICollection<ListaDesejo> ListaDesejos { get; set; }
-        public ICollection<Categoria> Categorias { get; set; }
-        public ICollection<Tag> Tags { get; set; }
+        public ICollection<Imagem> Imagens { get; set; } = [];
+        public ICollection<Avaliacao> Avaliacoes { get; set; } = [];
+        public ICollection<ItemCarrinho> ItensCarrinho { get; set; } = [];
+        public ICollection<Pedido> Pedidos { get; set; } = [];
+        public ICollection<ListaDesejo> ListaDesejos { get; set; } = [];
+        public ICollection<Categoria> Categorias { get; set; } = [];
+        public ICollection<Tag> Tags { get; set; } = [];
     }
 }
