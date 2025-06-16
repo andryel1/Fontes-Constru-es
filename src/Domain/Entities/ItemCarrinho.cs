4919d1c8f68a229f11403028abbf5a534c93b854
Andryel <andryelbonfim01@gmail.com>
2025-06-16 01:57:14 -0300

    feat: atualiza entidade ItemCarrinho

--- src/Domain/Entities/ItemCarrinho.cs
+++ src/Domain/Entities/ItemCarrinho.cs
@@ -1,12 +1,11 @@
-
 namespace Ecommerce.Domain.Entities
 { 
     public class ItemCarrinho : Entitybase
     {
         public int Quantidade { get; set; }
-        public required double Preco { get; set; }
-        public required Produto Produto { get; set; } 
-        public required Carrinho Carrinho { get; set; } 
+        public double Preco { get; set; }
+        public Produto? Produto { get; set; } 
+        public Carrinho? Carrinho { get; set; } 
         public int CarrinhoId { get; set; } 
         public int ProdutoId { get; set; } 
     }
