d57ad9d3a94a8f1e636dc1f08298b6e9706d5d17
Andryel <andryelbonfim01@gmail.com>
2025-06-16 01:57:14 -0300

    feat: atualiza entidade Desconto

--- src/Domain/Entities/Desconto.cs
+++ src/Domain/Entities/Desconto.cs
@@ -2,11 +2,11 @@ namespace Ecommerce.Domain.Entities
 {
     public class Desconto : Entitybase
     {
-        public required double Valor { get; set; }
-        public required DateTime DataInicio { get; set; }
+        public double Valor { get; set; }
+        public DateTime DataInicio { get; set; }
         public DateTime DataFim { get; set; }
-        public required Produto Produto { get; set; } 
-         public bool Ativo { get; set; }
+        public Produto? Produto { get; set; } 
+        public bool Ativo { get; set; }
         public int ProdutoId { get; set; }
     }
 }
