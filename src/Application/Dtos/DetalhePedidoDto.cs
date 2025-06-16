d402fc077307cf95329dd0132d0bee17c4da05ac
Andryel <andryelbonfim01@gmail.com>
2025-06-16 03:15:14 -0300

    feat(dto): atualiza DetalhePedidoDto com novos campos

--- src/Application/Dtos/DetalhePedidoDto.cs
+++ src/Application/Dtos/DetalhePedidoDto.cs
@@ -1,12 +1,9 @@
-using Ecommerce.Domain.Entities;
-
 namespace Application.Dtos;
 
-public class DetalhePedidoDto
-{
-        public string? NomeProduto { get; set; }
-        public int Quantidade { get; set; }
-        public double Preco { get; set; }
-        public double SubTotal { get; set; }
-        public int PedidoId { get; set; }
-}
+public record DetalhePedidoDto(
+        string? NomeProduto,
+        int Quantidade,
+        double Preco,
+        double SubTotal,
+        int PedidoId
+);
