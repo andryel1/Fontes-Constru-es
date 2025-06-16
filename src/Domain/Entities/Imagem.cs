04c804e48a848f9382ad1b2bf5126a6bba7e66f2
Andryel <andryelbonfim01@gmail.com>
2025-06-16 01:57:14 -0300

    feat: atualiza entidade Imagem

--- src/Domain/Entities/Imagem.cs
+++ src/Domain/Entities/Imagem.cs
@@ -1,12 +1,10 @@
-
 namespace Ecommerce.Domain.Entities
 {
     public class Imagem : Entitybase
     {
-        public required Uri Url { get; set; }
-        public required string Descricao { get; set; }
-        public required Produto Produto { get; set; }
+        public Uri? Url { get; set; }
+        public string? Descricao { get; set; }
+        public Produto? Produto { get; set; }
         public int ProdutoId { get; set; }
-        
     }
 }
