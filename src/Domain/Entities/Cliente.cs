b50f5d42d81f02b09dadf0bf7bc41ad0436c4079
Andryel <andryelbonfim01@gmail.com>
2025-06-16 01:57:14 -0300

    feat: atualiza entidade Cliente

--- src/Domain/Entities/Cliente.cs
+++ src/Domain/Entities/Cliente.cs
@@ -2,14 +2,14 @@ namespace Ecommerce.Domain.Entities
 {
     public class Cliente : Entitybase
     {
-        public required string Nome { get; set; }
-        public required int Cpf { get; set; }
-        public required string Password { get; set; }
-        public required string Telefone { get; set; }
-        public required string Email { get; set; }
+        public string? Nome { get; set; }
+        public int Cpf { get; set; }
+        public string? Password { get; set; }
+        public string? Telefone { get; set; }
+        public string? Email { get; set; }
         public DateTime DataNascimento { get; set; }
         public Usuario? Usuarios { get; set; }
-    
+
+        ICollection<ListaDesejo> ListaDesejos { get; set; } = [];
     }
-    
 }
