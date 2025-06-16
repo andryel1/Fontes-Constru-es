ce3aaf88e046f12a5bd339096a6eb0f1a19c022d
Andryel <andryelbonfim01@gmail.com>
2025-06-16 01:57:14 -0300

    feat: atualiza entidade DetalhePedido

--- src/Domain/Entities/DetalhePedido.cs
+++ src/Domain/Entities/DetalhePedido.cs
@@ -1,13 +1,12 @@
-
 namespace Ecommerce.Domain.Entities
 {
     public class DetalhePedido : Entitybase
     {
-        public required string NomeProduto { get; set; }
-        public required int Quantidade { get; set; }
-        public required double Preco { get; set; }
+        public string? NomeProduto { get; set; }
+        public int Quantidade { get; set; }
+        public double Preco { get; set; }
         public double SubTotal { get; set; }
-        public required Pedido Pedido { get; set; }
+        public Pedido? Pedido { get; set; }
         public int PedidoId { get; set; }
     }
 }
