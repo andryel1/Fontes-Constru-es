44be061651e89001c4e28f258b55dc1fea64dfbe
Andryel <andryelbonfim01@gmail.com>
2025-06-16 03:15:14 -0300

    chore(context): atualiza contexto para refletir novas entidades

--- src/Infrastructure/Context.cs
+++ src/Infrastructure/Context.cs
@@ -4,27 +4,25 @@ using Infrastructure.Configurations;
 
 namespace Infrastructure
 {
-    public class Context : DbContext
+    public class Context(DbContextOptions<Context> options) : DbContext(options), DbContext
     {
-        public Context(DbContextOptions<Context> options) : base(options)
-        {
-        }
-
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Pedido> Pedidos { get; set; }
         public DbSet<Avaliacao> Avaliacoes { get; set; }
         public DbSet<Carrinho> Carrinhos { get; set; }
         public DbSet<ItemCarrinho> ItensCarrinho { get; set; }
+        public DbSet<ListaDesejo> ListaDesejo{ get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            
+
             modelBuilder.ApplyConfiguration(new AvaliacaoConfiguration());
             modelBuilder.ApplyConfiguration(new CarrinhoConfiguration());
             modelBuilder.ApplyConfiguration(new PedidoConfiguration());
             modelBuilder.ApplyConfiguration(new ProdutoConfiguration());
             modelBuilder.ApplyConfiguration(new TagConfiguration());
+            modelBuilder.ApplyConfiguration(new ListaDesejoConfiguration());
         }
     }
 }
