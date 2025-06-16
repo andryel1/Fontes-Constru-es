3b8e21a01720d3c9102183daa9ad7f23714ec8a7
Andryel <andryelbonfim01@gmail.com>
2025-06-16 06:05:28 -0300

    chore: adicionada nova pasta Resources à aplicação

---
+++ src/Application/Resources/Messages/PagamentoMessages.cs
@@ -0,0 +1,46 @@
+using System.Globalization;
+using System.Resources;
+
+namespace Resources.Messages;
+
+public static class PagamentoMessages
+{
+    private static readonly ResourceManager resourceManager =
+        new("Resources.Messages.PagamentoMessages", typeof(PagamentoMessages).Assembly);
+
+    public static string ValorObrigatorio =>
+        resourceManager.GetString("ValorObrigatorio", CultureInfo.CurrentUICulture) ?? "[ValorObrigatorio]";
+
+    public static string ValorMaiorQueZero =>
+        resourceManager.GetString("ValorMaiorQueZero", CultureInfo.CurrentUICulture) ?? "[ValorMaiorQueZero]";
+
+    public static string TipoPagamentoObrigatorio =>
+        resourceManager.GetString("TipoPagamentoObrigatorio", CultureInfo.CurrentUICulture) ?? "[TipoPagamentoObrigatorio]";
+
+    public static string TipoPagamentoInvalido =>
+        resourceManager.GetString("TipoPagamentoInvalido", CultureInfo.CurrentUICulture) ?? "[TipoPagamentoInvalido]";
+
+    public static string StatusObrigatorio =>
+        resourceManager.GetString("StatusObrigatorio", CultureInfo.CurrentUICulture) ?? "[StatusObrigatorio]";
+
+    public static string StatusInvalido =>
+        resourceManager.GetString("StatusInvalido", CultureInfo.CurrentUICulture) ?? "[StatusInvalido]";
+
+    public static string DataPagamentoObrigatoria =>
+        resourceManager.GetString("DataPagamentoObrigatoria", CultureInfo.CurrentUICulture) ?? "[DataPagamentoObrigatoria]";
+
+    public static string DataPagamentoInvalida =>
+        resourceManager.GetString("DataPagamentoInvalida", CultureInfo.CurrentUICulture) ?? "[DataPagamentoInvalida]";
+
+    public static string ParcelamentoObrigatorio =>
+        resourceManager.GetString("ParcelamentoObrigatorio", CultureInfo.CurrentUICulture) ?? "[ParcelamentoObrigatorio]";
+
+    public static string ParcelamentoIdObrigatorio =>
+        resourceManager.GetString("ParcelamentoIdObrigatorio", CultureInfo.CurrentUICulture) ?? "[ParcelamentoIdObrigatorio]";
+
+    public static string PedidosObrigatorio =>
+        resourceManager.GetString("PedidosObrigatorio", CultureInfo.CurrentUICulture) ?? "[PedidosObrigatorio]";
+
+    public static string PedidosMin1 =>
+        resourceManager.GetString("PedidosMin1", CultureInfo.CurrentUICulture) ?? "[PedidosMin1]";
+} 
