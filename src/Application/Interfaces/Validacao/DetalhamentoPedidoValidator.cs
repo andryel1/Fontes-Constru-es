ca8cfcd89333ae1a9c5c0a4c342560a3197e68cc
Andryel <andryelbonfim01@gmail.com>
2025-06-16 03:15:14 -0300

    feat(validation): cria validações para DetalhamentoPedido

--- src/Application/Interfaces/Validacao/DetalhamentoPedidoValidator.cs
+++ src/Application/Interfaces/Validacao/DetalhamentoPedidoValidator.cs
@@ -8,13 +8,5 @@ public class DetalhamentoPedidoValidator : AbstractValidator<DetalhePedidoDto>
 {
     public DetalhamentoPedidoValidator()
     {
-        RuleFor(x => x.PedidoId)
-            .NotEmpty().WithMessage(DetalheDoPedidoMessages.PedidoIdObrigatorio);
-
-        RuleFor(x => x.ProdutoId)
-            .NotEmpty().WithMessage(DetalheDoPedidoMessages.ProdutoIdObrigatorio);
-
-        RuleFor(x => x.Quantidade)
-            .GreaterThan(0).WithMessage(DetalheDoPedidoMessages.QuantidadeMaiorQueZero);
     }
 }
