30620891444eb5d85c50846e93aa636fb8134036
Andryel <andryelbonfim01@gmail.com>
2025-06-16 03:15:14 -0300

    refactor(repo): melhora interface de envio

--- src/Application/Interfaces/Repository/IInformacoesEnvioRepository.cs
+++ src/Application/Interfaces/Repository/IInformacoesEnvioRepository.cs
@@ -5,9 +5,5 @@ namespace Application.Interfaces.Repository;
 
 public interface IInformacoesEnvioRepository : IInterfaceBaseRepository<InformacoesEnvioDto, InformacoesEnvio>
 {
-    Task<InformacoesEnvioDto> ObterInformacoesEnvioPorId(int id);
-    Task<IEnumerable<InformacoesEnvioDto>> ObterTodosInformacoesEnvio();
-    Task<bool> AdicionarInformacoesEnvio(InformacoesEnvioDto informacoesEnvioDto);
-    Task<bool> AtualizarInformacoesEnvio(InformacoesEnvioDto informacoesEnvioDto);
-    Task<bool> DeletarInformacoesEnvio(int id);
+  
 }
