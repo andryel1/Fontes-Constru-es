9eb230561f9f50886a176e50f05e84743828e7d1
Andryel <andryelbonfim01@gmail.com>
2025-06-16 01:57:14 -0300

    feat: adiciona configuração de ListaDesejo

---
+++ src/Infrastructure/Configurations/ListaDesejoConfiguration.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ecommerce.Domain.Entities;
+
+namespace Infrastructure.Configurations
+{
+    public class PedidoConfiguration : IEntityTypeConfiguration<Pedido>
+    {
+        public void Configure(EntityTypeBuilder<Pedido> builder)
+        {
+            builder.ToTable("ListaDesejo");
+
+            builder.HasKey(ld => ld.Id);
+
+            builder.Property(ld => ld.DataAdicionada)
+                   .IsRequired();
+
+            builder.HasMany(ld => ld.Produtos)
+                   .WithMany();
+
+            builder.HasOne(ld => ld.Clientes)
+                   .WithMany()
+                   .HasForeignKey(ld => ld.ClienteId)
+                   .IsRequired();
+        }
+    }
+}
