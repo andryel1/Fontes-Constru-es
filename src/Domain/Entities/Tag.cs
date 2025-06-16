45677b9722a14341dc6a04833891a2acacb747b3
Andryel <andryelbonfim01@gmail.com>
2025-06-16 01:57:14 -0300

    feat: atualiza entidade Tag

--- src/Domain/Entities/Tag.cs
+++ src/Domain/Entities/Tag.cs
@@ -2,9 +2,8 @@ namespace Ecommerce.Domain.Entities
 {
     public class Tag : Entitybase
     {
-        public required string Nome { get; set; }
-        public required Produto Produto { get; set; }
+        public string? Nome { get; set; }
+        public Produto? Produto { get; set; }
         public int ProdutoId { get; set; }
-
     }
 }   
