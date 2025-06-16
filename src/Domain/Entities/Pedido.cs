0f044c6d4bb37a042eac2e2c9c95b5948bada541
Andryel <andryelbonfim01@gmail.com>
2025-06-16 01:57:14 -0300

    feat: atualiza entidade Pedido

--- src/Domain/Entities/Pedido.cs
+++ src/Domain/Entities/Pedido.cs
@@ -3,11 +3,13 @@ namespace Ecommerce.Domain.Entities
     public class Pedido : Entitybase
     {
         public DateTime DataPedido { get; set; }
-        public required string Status { get; set; }
+        public string? Status { get; set; }
         public double ValorTotal { get; set; }
-        public required ICollection<Carrinho> Itens { get; set; }
-        public required ICollection<Cliente> Clientes { get; set; }
+        public ICollection<Carrinho> Itens { get; set; } = [];
+        public ICollection<Cliente> Clientes { get; set; } = [];
         public DateTime? DataEnvio { get; set; }
         public DateTime? DataEntrega { get; set; }
+        public ICollection<Pagamento> Pagamentos { get; set; } = [];
+        public ICollection<InformacoesEnvio> InformacoesEnvio { get; set; } = [];
     }
 }
