a59886b3563f8de2645cc5842a86b707a512216b
Andryel <andryelbonfim01@gmail.com>
2025-06-22 19:16:24 -0300

    Adiciona Infrastructure/Configurations/ProdutoConfuguration.cs

--- src/Infrastructure/Configurations/ProdutoConfuguration.cs
+++ src/Infrastructure/Configurations/ProdutoConfuguration.cs
@@ -8,7 +8,26 @@ namespace Infrastructure.Configurations
     {
         public void Configure(EntityTypeBuilder<Produto> builder)
         {
-          
+            builder.ToTable("Produtos");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Nome)
+            .IsRequired()
+            .HasMaxLength(500);
+
+            builder.Property(p => p.Descricao)
+            .IsRequired()
+            .HasMaxLength(500);
+
+            builder.Property(p => p.Estoque)
+            .IsRequired()
+            .HasColumnType("int");
+
+            builder.Property(p => p.Preco)
+            .IsRequired()
+            .HasColumnType("decimal(10, 2)");
+            
         }
     }
 }
