558b592570ff4268c6a1db4d97302cefde370a94
Andryel <andryelbonfim01@gmail.com>
2025-06-27 23:58:04 -0300

    Ajustes na entidade Pedido

--- src/Domain/Entities/Pedido.cs
+++ src/Domain/Entities/Pedido.cs
@@ -1,15 +1,21 @@
 namespace Ecommerce.Domain.Entities
 {
-    public class Pedido : Entitybase
-    {
-        public DateTime DataPedido { get; set; }
-        public string? Status { get; set; }
-        public double ValorTotal { get; set; }
-        public ICollection<Carrinho> Itens { get; set; } = [];
-        public ICollection<Cliente> Clientes { get; set; } = [];
-        public DateTime? DataEnvio { get; set; }
-        public DateTime? DataEntrega { get; set; }
-        public ICollection<Pagamento> Pagamentos { get; set; } = [];
-        public ICollection<InformacoesEnvio> InformacoesEnvio { get; set; } = [];
-    }
+   public class Pedido : Entitybase
+{
+    public DateTime DataPedido { get; set; }
+    public string? Status { get; set; }
+    public double ValorTotal { get; set; }
+
+    public ICollection<DetalhePedido> Detalhes { get; set; } = []; 
+
+    public ICollection<Carrinho> Itens { get; set; } = [];
+    public ICollection<Cliente> Clientes { get; set; } = [];
+
+    public DateTime? DataEnvio { get; set; }
+    public DateTime? DataEntrega { get; set; }
+
+    public ICollection<Pagamento> Pagamentos { get; set; } = [];
+    public ICollection<InformacoesEnvio> InformacoesEnvio { get; set; } = [];
+}
+
 }
