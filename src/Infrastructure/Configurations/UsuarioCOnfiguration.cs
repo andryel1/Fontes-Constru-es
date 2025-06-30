be45de04000131463c01acaa6ff067a23545af25
Andryel <andryelbonfim01@gmail.com>
2025-06-29 23:19:40 -0300

    Corrigir/atualizar configuração de Usuario

--- src/Infrastructure/Configurations/UsuarioCOnfiguration.cs
+++ src/Infrastructure/Configurations/UsuarioCOnfiguration.cs
@@ -7,6 +7,21 @@ public class UsuarioConfiguration : IEntityTypeConfiguration<Usuario>
 {
     public void Configure(EntityTypeBuilder<Usuario> builder)
     {
-    
+        builder.ToTable("Usuario");
+
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.UserName);
+
+        builder.Property(u => u.Password);
+
+        builder.Property(u => u.Email);
+
+        builder.Property(u => u.DataNascimento)
+        .HasColumnType("datetime");
+
+        builder.HasOne(u => u.Cliente)
+        .WithOne(cliente => cliente.Usuario)
+        .HasForeignKey<Cliente>(cliente => cliente.UsuarioId);
     }
 }
