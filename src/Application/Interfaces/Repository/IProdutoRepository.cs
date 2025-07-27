b34435986a1dbedd74b4cb4167115ad6ab043606
Andryel <andryelbonfim01@gmail.com>
2025-07-27 19:43:06 -0300

    Update IProdutoRepository.cs

--- src/Application/Interfaces/Repository/IProdutoRepository.cs
+++ src/Application/Interfaces/Repository/IProdutoRepository.cs
@@ -1,12 +1,18 @@
-using Ecommerce.Domain.Entities;
 using Application.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace Application.Interfaces.Repository
+public interface IProdutoRepository
 {
-    public interface IProdutoRepository : IInterfaceBaseRepository<ProdutoDto, Produto>
-    {
-        Task<List<ProdutoDto>> ObterProdutosPorCategoria(string categoria);
-        Task<List<ProdutoDto>> ObterProdutosPorNome(string nome);
-    }
+    Task<ProdutoDto> VerificarProdutoExistente(string nome);
+    Task<ProdutoDto> NomeNaoPodeSerVazioOuNUlo(string nome);
+    Task<ProdutoDto> DescricaoNaoPodeSerVazioOuNulo(string descricao);
+    Task<ProdutoDto> PrecoNaoPodeSerVazioOuNulo(decimal preco);
+    Task<List<ProdutoDto>> ObterProdutosPorCategoria(string categoria);
+    Task<List<ProdutoDto>> ObterProdutosPorNome(string nome);
+    Task<ProdutoDto> Adicionar(ProdutoDto dto);
+    Task<ProdutoDto> Atualizar(ProdutoDto dto);
+    Task<bool> Deletar(int id);
+    Task<ProdutoDto> ObterPorId(int id);
+    Task<List<ProdutoDto>> ObterTodos();
 }
-
