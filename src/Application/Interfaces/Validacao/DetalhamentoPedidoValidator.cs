052488661e5533282126d0ef3d8bacf8c51dab72
Andryel <andryelbonfim01@gmail.com>
2025-08-18 00:16:01 -0300

    {mensagem_DetalhamentoPedidoValidator}

--- src/Application/Interfaces/Validacao/DetalhamentoPedidoValidator.cs
+++ src/Application/Interfaces/Validacao/DetalhamentoPedidoValidator.cs
@@ -1,11 +1,13 @@
 using FluentValidation;
 using Application.Dtos;
+using Microsoft.Extensions.Localization;
 
 namespace Application.Interfaces.Validacao;
 
 public class DetalhamentoPedidoValidator : AbstractValidator<DetalhePedidoDto>
 {
-    public DetalhamentoPedidoValidator()
+    public DetalhamentoPedidoValidator(IStringLocalizer localizer)
     {
+        ArgumentNullException.ThrowIfNull(localizer);
     }
 }
