9036d0a5a1c2484e0df6e17b6989e7511d014bd0
Andryel <andryelbonfim01@gmail.com>
2025-08-18 23:41:46 -0300

    Update TagValidator.cs

--- src/Application/Interfaces/Validacao/TagValidator.cs
+++ src/Application/Interfaces/Validacao/TagValidator.cs
@@ -3,15 +3,21 @@ using Application.Dtos;
 using Resources.Messages;
 using Ecommerce.Domain.Entities;
 using Microsoft.Extensions.Localization;
+using Org.BouncyCastle.Math.EC.Rfc7748;
 
 
 namespace Application.Interfaces.Validacao
 {
     public class TagValidator : AbstractValidator<TagDto>
     {
-        public TagValidator(IStringLocalizer localizer)
+        public TagValidator(IStringLocalizer<TagDto> localizer)
         {
             ArgumentNullException.ThrowIfNull(localizer);
+
+            RuleFor(x => x.Nome)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage(localizer[TagMessages.NomeObrigatorio]);
         }
     }
 }
