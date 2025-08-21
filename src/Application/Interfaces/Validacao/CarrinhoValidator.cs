8626b5f61db9c5b15e5a68e9c98864047404a124
Andryel <andryelbonfim01@gmail.com>
2025-08-21 17:28:44 -0300

    {mensagem_CarrinhoValidator}

--- src/Application/Interfaces/Validacao/CarrinhoValidator.cs
+++ src/Application/Interfaces/Validacao/CarrinhoValidator.cs
@@ -9,12 +9,20 @@ public class CarrinhoValidator : AbstractValidator<CarrinhoDto>
     public CarrinhoValidator(IStringLocalizer localizer)
     {
         ArgumentNullException.ThrowIfNull(localizer);
+        RuleFor(x => x.Id);
         RuleFor(x => x.Itens)
             .NotNull().WithMessage(localizer[ItemCarrinhoMessages.ProdutoObrigatorio])
-            .Must(itens => itens != null && itens.Any())
+            .Must(itens => itens != null && itens.Count > 0)
             .WithMessage(localizer[ItemCarrinhoMessages.ProdutoObrigatorio]);
 
         RuleFor(x => x.ClienteId)
-            .NotEmpty().WithMessage(localizer[ItemCarrinhoMessages.CarrinhoObrigatorio]);
+            .NotEmpty()
+            .WithMessage(localizer[ItemCarrinhoMessages.CarrinhoObrigatorio]);
+        RuleFor(x => x.PedidoId);
+        RuleFor(x => x.PrecoTotal)
+        .NotNull()
+        .NotEmpty()
+        .GreaterThan(0);
+        RuleFor(x => x.DataCriacao);
     }
 }
