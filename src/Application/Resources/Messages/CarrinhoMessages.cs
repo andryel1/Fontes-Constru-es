ae2bebbd2d356ecf3b17140061818795d9ddc39f
Andryel <andryelbonfim01@gmail.com>
2025-08-23 23:07:10 -0300

    Atualiza CarrinhoMessages.cs

--- src/Application/Resources/Messages/CarrinhoMessages.cs
+++ src/Application/Resources/Messages/CarrinhoMessages.cs
@@ -1,8 +1,26 @@
 using System.Globalization;
 using System.Resources;
 
-namespace Resources.Messages;
+namespace Resources.Messages
+{
     public static class CarrinhoMessages
     {
-        
-    } 
+        private static readonly ResourceManager resourceManager =
+            new("Resources.Messages.CarrinhoMessages", typeof(CarrinhoMessages).Assembly);
+
+        public static string NomeObrigatorio =>
+            resourceManager.GetString("NomeObrigatorio", CultureInfo.CurrentUICulture) ?? "[NomeObrigatorio]";
+
+        public static string ItemInvalido =>
+            resourceManager.GetString("ItemInvalido", CultureInfo.CurrentUICulture) ?? "[ItemInvalido]";
+
+        public static string ModificacaoNaoPermitida =>
+            resourceManager.GetString("ModificacaoNaoPermitida", CultureInfo.CurrentUICulture) ?? "[ModificacaoNaoPermitida]";
+
+        public static string ItemAdicionadoComSucesso =>
+            resourceManager.GetString("ItemAdicionadoComSucesso", CultureInfo.CurrentUICulture) ?? "[ItemAdicionadoComSucesso]";
+
+        public static string ItemRemovidoComSucesso =>
+            resourceManager.GetString("ItemRemovidoComSucesso", CultureInfo.CurrentUICulture) ?? "[ItemRemovidoComSucesso]";
+    }
+}
