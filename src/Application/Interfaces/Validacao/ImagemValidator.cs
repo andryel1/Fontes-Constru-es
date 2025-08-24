ab88fbea806d35e7b8b34101c22f666c2646526c
Andryel <andryelbonfim01@gmail.com>
2025-08-23 23:07:10 -0300

    Atualiza ImagemValidator.cs

--- src/Application/Interfaces/Validacao/ImagemValidator.cs
+++ src/Application/Interfaces/Validacao/ImagemValidator.cs
@@ -11,13 +11,15 @@ public class ImagemValidator : AbstractValidator<ImagemDto>
         ArgumentNullException.ThrowIfNull(localizer);
 
         RuleFor(x => x.Id);
+
         RuleFor(x => x.Url)
-            .NotEmpty().WithMessage(ImagemMessages.UrlObrigatoria)
+            .NotEmpty().WithMessage(localizer[ImagemMessages.UrlObrigatoria])
             .Must((_, url) => url != null && Uri.IsWellFormedUriString(url.AbsoluteUri, UriKind.Absolute))
             .WithMessage(localizer[ImagemMessages.UrlInvalida]);
 
         RuleFor(x => x.ProdutoId)
-            .NotEmpty().WithMessage(localizer[ImagemMessages.ProdutoIdObrigatorio]);
+            .NotEmpty().WithMessage(localizer[ImagemMessages.ProdutoIdObrigatorio])
+            .GreaterThan(0).WithMessage(localizer[ImagemMessages.ProdutoIdInvalido]);
 
         RuleFor(x => x.Produto);
     }
