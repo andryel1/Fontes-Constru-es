a473529b4076d2a338e319d2c42c66369ac264d5
Andryel <andryelbonfim01@gmail.com>
2025-08-24 23:32:42 -0300

    fix(interface): atualiza métodos em IDescontoRepository

--- src/Application/Interfaces/Repository/IDescontoRepository.cs
+++ src/Application/Interfaces/Repository/IDescontoRepository.cs
@@ -5,7 +5,6 @@ namespace Application.Interfaces.Repository;
 
 public interface IDescontoRepository : IInterfaceBaseRepository<DescontoDto, Desconto>
 {
-    Task<DescontoDto> ObterDescontoPorId(int id);
     Task<IEnumerable<DescontoDto>> ObterDescontosAtivos();
     Task<IEnumerable<DescontoDto>> ObterDescontosInativos();
     Task<IEnumerable<DescontoDto>> ObterDescontosExpirados();
